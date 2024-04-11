de0386fe5cbc9c66ae24e14e8664ba2e5bdf03e6
zaynom <ouaziz0626@gmail.com>
2024-04-11 17:08:38 +0000

    task 0: Pascals Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    array = []
+    for i in range(n):
+        row = []
+        for j in range(i+1):
+            if j == 0 or j == i:
+                row.append(1)
+            elif i > 1:
+                row.append(array[i-1][j] + array[i-1][j-1])
+        array.append(row)
+
+    return array
