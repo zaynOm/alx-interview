8563ee66d8554cc4d6d1106f0a22e86efefbe869
zaynom <ouaziz0626@gmail.com>
2024-05-03 01:30:38 +0100

    boo

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,45 +4,49 @@ import re
 import sys
 
 
-file_size = 0
-status_code = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0,
-}
-
-
-def print_data():
+def print_data(file_size, status_codes):
     "Print the statistics"
     print(f"File size: {file_size}")
-    for code, count in sorted(status_code.items()):
+    for code, count in sorted(status_codes.items()):
         if count > 0:
             print(f"{code}: {count}", flush=True)
 
 
-try:
-    for i, line in enumerate(sys.stdin):
-        if i != 0 and i % 10 == 0:
-            print_data()
+def main():
+    file_size = 0
+    status_codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
 
-        line = line.rstrip()
-        pattern = r"""
-            [\d.]+
-            \s-\s
-            \[[\d\- :.]+\]
-            \s"GET\s\/projects\/260\sHTTP\/1\.1"\s
-            (\d{3})
-            \s(\d+)
-        """
-        res = re.search(pattern, line, re.X)
-        code, size = res.groups()
-        if code in status_code:
-            file_size += int(size)
-            status_code[code] = status_code.get(code, 0) + 1
-except KeyboardInterrupt:
-    print_data()
+    try:
+        for i, line in enumerate(sys.stdin):
+            if i != 0 and i % 10 == 0:
+                print_data(file_size, status_codes)
+
+            line = line.rstrip()
+            pattern = r"""
+                [\d.]+
+                \s-\s
+                \[[\d\- :.]+\]
+                \s"GET\s\/projects\/260\sHTTP\/1\.1"\s
+                (\d{3})
+                \s(\d+)
+            """
+            res = re.search(pattern, line, re.X)
+            code, size = res.groups()
+            if code in status_codes:
+                file_size += int(size)
+                status_codes[code] = status_codes.get(code, 0) + 1
+    except (KeyboardInterrupt, EOFError):
+        print_data(file_size, status_codes)
+
+
+if __name__ == "__main__":
+    main()
