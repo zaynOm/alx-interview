d7f8a6b352dfd3232691d8b6c7ec45abaf2aba95
zaynom <ouaziz0626@gmail.com>
2024-05-22 12:43:53 +0100

    Task 0: Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,15 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-const argv = process.argv;
+const { argv } = require('process');
 
 const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
 request(url, async (err, res, body) => {
   if (err) throw err;
   const chars = JSON.parse(body).characters;
   for (const charUrl of chars) {
-    request(charUrl, async (err, _, body) => {
-      if (err) throw err;
-      console.log(JSON.parse(body).name);
+    const { name } = await new Promise((resolve, reject) => {
+      request(charUrl, async (err, _, body) => {
+        if (err) throw err;
+        resolve(JSON.parse(body));
+      });
     });
+    console.log(name);
   }
 });
