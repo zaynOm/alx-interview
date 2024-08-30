35a4a1a948c77b2d945113221b41ec57a3c470de
zaynom <ouaziz0626@gmail.com>
2024-08-30 01:50:00 +0100

    Task 0:  Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env3
+"""Island Perimeter"""
+
+
+def island_perimeter(grid: list) -> int:
+    """returns the perimeter of the island described in grid"""
+    p = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                if j == 0 or grid[i][j - 1] == 0:
+                    p += 1
+                if j == cols - 1 or grid[i][j + 1] == 0:
+                    p += 1
+                if i == 0 or grid[i - 1][j] == 0:
+                    p += 1
+                if i == rows - 1 or grid[i + 1][j] == 0:
+                    p += 1
+    return p
